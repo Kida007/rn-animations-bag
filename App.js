970207622844c151130950dca64afec1dee2eba8
PIYUSH GUPTA <piyush.gupta111019@gmail.com>
2018-09-29 03:47:50 +0530

    added flow

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
+//@flow
+
 import React, { Component } from 'react' ;
 import { RotatingSquare, Spring, SequenceAnimation } from './examples' ;
-class App extends Component {
+import { Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation' ;
+
+type Props = {
+
+}
+
+class App extends Component<Props> {
+
+  multiply(n1: number,n2: number) :number {
+    return n1 * n2 ;
+  }
 
   render(){
+
+    let x: number = 123;
+
     return(
-      <SequenceAnimation />
+      <View style={{ justifyContent:'center', alignItems:'center', flex:1}}>
+          <Text>{this.multiply(10,10)}</Text>
+      </View>
     )
   }
 }
