39686b37f53ab035a186b2b263e8cafd5e16f986
PIYUSH GUPTA <piyush.gupta111019@gmail.com>
2018-12-02 03:44:16 +0530

    apple pay wallet ver 2

--- examples/AppleWallet.js
+++ examples/AppleWallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { View, SafeAreaView, Text, Animated, StyleSheet, ScrollView } from 'react-native';
+import { View, SafeAreaView, Text, Animated, StyleSheet, Dimensions, PanResponder } from 'react-native';
 import cards from './data/cards';
 
 
 const cardHeight = 250;
-const cardTitleOffset = 45;
+const cardTitle = 45;
 const cardPadding = 10;
+const { height } = Dimensions.get('window');
+
 
 class Card extends React.Component {
   render() {
@@ -14,23 +16,46 @@ class Card extends React.Component {
   }
 }
 
+
 // eslint-disable-next-line react/no-multi-comp
 class AppleWallet extends React.Component {
     state = {
       y: new Animated.Value(0),
     };
 
+
+    constructor(props) {
+      super(props);
+
+      const { y } = this.state;
+
+      this.panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderMove: (e, gestureState) => {
+          y.setValue(-1 * gestureState.dy);
+          console.log(y);
+        },
+        onPanResponderRelease: () => {
+          Animated.timing(y, {
+            duration: 100,
+            toValue: 0,
+          }).start();
+        },
+      });
+    }
+
     render() {
       const { y } = this.state;
       return (
         <SafeAreaView style={styles.root}>
-          <View style={styles.container}>
+          <View style={styles.container} {...this.panResponder.panHandlers}>
             <View style={StyleSheet.absoluteFill}>
               {
                 cards.map((card, i) => {
                   const translateY = y.interpolate({
-                    inputRange: [-cardHeight, 0, cardPadding * 7],
-                    outputRange: [cardHeight * i, (cardHeight - cardTitleOffset) * -i, (cardHeight - 10) * -i],
+                    inputRange: [-(cardHeight * 10), 0, 5 * cardPadding],
+                    outputRange: [cardHeight * 10 * (i / 1.5), (cardHeight - cardTitle) * -i, (cardHeight - cardPadding) * -i],
                     extrapolateRight: 'clamp',
                   });
                   return (
@@ -41,18 +66,6 @@ class AppleWallet extends React.Component {
                 })
                 }
             </View>
-            <Animated.ScrollView
-              scrollEventThrottle={16}
-              onScroll={Animated.event(
-                [
-                  {
-                    nativeEvent: {
-                      contentOffset: { y },
-                    },
-                  },
-                ],
-              )}
-            />
           </View>
         </SafeAreaView>
       );
@@ -68,6 +81,9 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+  },
+  content: {
+    height: height * 2,
   },
   card: {
     height: cardHeight,
